f153cc40da625f10083c31b0c204705863a4625e
Christopher Cortazo <cortazo.christopher@gmail.com>
2025-03-06 00:16:40 -0500

    Final Version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,12 @@ import { motion } from "framer-motion";
 
 export default function SecretLetter() {
   const [answer, setAnswer] = useState("");
-  const [isCorrect1, setIsCorrect1] = useState(true);
-  const [isCorrect2, setIsCorrect2] = useState(true);
+  const [isCorrect1, setIsCorrect1] = useState(false);
+  const [isCorrect2, setIsCorrect2] = useState(false);
   const [isTimeReached, setIsTimeReached] = useState(false);
   const [isNext, setIsNext] = useState(false);
   const [timeLeft, setTimeLeft] = useState(0);
-  const revealDate = useMemo(() => new Date("2025-03-01T00:00:00"), []);
+  const revealDate = useMemo(() => new Date("2025-03-07T00:00:00"), []);
 
   useEffect(() => {
     const checkTime = () => {
@@ -62,7 +62,7 @@ export default function SecretLetter() {
   const handleSubmitFirst = () => {
     if (answer.toLowerCase().includes("solecito")) {
       setIsCorrect1(true);
-      setAnswer(""); // Reset answer field for next question
+      setAnswer("");
     } else {
       setIsCorrect1(false);
     }
@@ -77,7 +77,6 @@ export default function SecretLetter() {
   };
 
   const handleNext = () => {
-    console.log("Next button clicked");
     setIsNext(true);
   };
 
@@ -109,9 +108,6 @@ export default function SecretLetter() {
             >
               🔒 What is my nickname? 🔒
             </Typography>
-            {/* <Typography variant="body1" sx={{ marginTop: 2,  }}>
-              What is my nickname?
-            </Typography> */}
             <Box
               sx={{
                 display: "flex",
@@ -119,8 +115,6 @@ export default function SecretLetter() {
                 alignItems: "center",
                 alignContent: "center",
                 width: "100%",
-                // marginTop: 2,
-                // marginLeft: 8
               }}
             >
               <TextField
@@ -142,7 +136,7 @@ export default function SecretLetter() {
                       color: "black",
                       fontFamily: "'Fresh Palm', cursive",
                       fontSize: "1.5rem",
-                    }, // White text for contrast
+                    },
                     "&::placeholder": { color: "rgba(255,255,255,0.7)" }, // Adjust placeholder
                   },
                 }}
@@ -188,9 +182,6 @@ export default function SecretLetter() {
             >
               🔒 What is your nickname? 🔒
             </Typography>
-            {/* <Typography variant="body1" sx={{ marginTop: 2,  }}>
-              What is my nickname?
-            </Typography> */}
             <Box
               sx={{
                 display: "flex",
@@ -198,12 +189,9 @@ export default function SecretLetter() {
                 alignItems: "center",
                 alignContent: "center",
                 width: "100%",
-                // marginTop: 2,
-                // marginLeft: 8
               }}
             >
               <TextField
-                // fullWidth
                 placeholder="Your Answer"
                 variant="outlined"
                 value={answer}
@@ -221,7 +209,7 @@ export default function SecretLetter() {
                       color: "black",
                       fontFamily: "'Fresh Palm', cursive",
                       fontSize: "1.5rem",
-                    }, // White text for contrast
+                    },
                     "&::placeholder": { color: "rgba(255,255,255,0.7)" }, // Adjust placeholder
                   },
                 }}
@@ -259,56 +247,69 @@ export default function SecretLetter() {
             transition={{ duration: 1 }}
           >
             <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                }}
-              >
-            <Card
               sx={{
-                padding: 2,
-                maxWidth: "90%",
-                textAlign: "center",
-                boxShadow: 3,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
               }}
             >
-              <CardContent>
-                <Typography variant="h5" sx={{ fontFamily: "'Shine in Valentine',Fresh Palm", fontWeight: "bold" }}>
-                  💌 A Heartfelt Letter for You
-                </Typography>
-                <Typography variant="body1" sx={{ marginTop: 2, fontFamily: "'Fresh Palm',cursive", fontSize: "1.2rem" }}>
-                  I loved the letter you wrote for me so I decided to do the
-                  same... but with a little spin. This is my way to show how
-                  much you mean to me (y luego que no seria ovio😂) y la
-                  primera cosa que pense era en forma de esto. Hope you like it
-                  mi florecita preciosa❤️
-                </Typography>
+              <Card
+                sx={{
+                  padding: 2,
+                  maxWidth: "90%",
+                  textAlign: "center",
+                  boxShadow: 3,
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      fontFamily: "'Shine in Valentine',Fresh Palm",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    💌 A Heartfelt Letter for You
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      marginTop: 2,
+                      fontFamily: "'Fresh Palm',cursive",
+                      fontSize: "1.2rem",
+                    }}
+                  >
+                    I loved the letter you wrote for me so I decided to do the
+                    same... but with a little spin. This is my way to show how
+                    much you mean to me (y luego que no seria ovio😂) y la
+                    primera cosa que pense era en forma de esto. Hope you like
+                    it mi florecita preciosa❤️
+                  </Typography>
 
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  sx={{
-                    marginTop: 2,
-                    padding: "10px 20px",
-                    fontSize: "1.2rem",
-                    fontFamily: "'Fresh Palm', cursive",
-                    borderRadius: "50px",
-                    textTransform: "none",
-                    background: "rgba(255, 105, 180, 0.7)",
-                    backdropFilter: "blur(8px)",
-                    boxShadow: "0 4px 10px rgba(255, 105, 180, 0.3)",
-                    "&:hover": {
-                      background: "rgba(255, 20, 147, 0.8)",
-                    },
-                  }}
-                  onClick={handleNext} // Show the letter when Next is clicked
-                >
-                  Next
-                </Button>
-              </CardContent>
-            </Card>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    sx={{
+                      marginTop: 2,
+                      padding: "10px 20px",
+                      fontSize: "1.2rem",
+                      fontFamily: "'Fresh Palm', cursive",
+                      borderRadius: "50px",
+                      textTransform: "none",
+                      background: "rgba(255, 105, 180, 0.7)",
+                      backdropFilter: "blur(8px)",
+                      boxShadow: "0 4px 10px rgba(255, 105, 180, 0.3)",
+                      "&:hover": {
+                        background: "rgba(255, 20, 147, 0.8)",
+                      },
+                    }}
+                    onClick={handleNext} // Show the letter when Next is clicked
+                  >
+                    Next
+                  </Button>
+                </CardContent>
+              </Card>
             </Box>
           </motion.div>
         ) : (
@@ -337,13 +338,17 @@ export default function SecretLetter() {
                   }}
                 >
                   <CardContent
-                  sx={{
-                    maxHeight: "500px", // Set a fixed height for the card content
-                    overflowY: "auto",  // Allow scrolling within the content
-                  }}>
+                    sx={{
+                      maxHeight: "500px", // Set a fixed height for the card content
+                      overflowY: "auto", // Allow scrolling within the content
+                    }}
+                  >
                     <Typography
                       variant="h5"
-                      sx={{ fontFamily: "'Shine in Valentine',Fresh Palm", fontWeight: "bold" }}
+                      sx={{
+                        fontFamily: "'Shine in Valentine',Fresh Palm",
+                        fontWeight: "bold",
+                      }}
                     >
                       💌 Happy Monthsary 💌
                     </Typography>
@@ -361,56 +366,104 @@ export default function SecretLetter() {
                       doesn’t it? It feels like just yesterday I was being so
                       awkward and didn’t know how to talk to you. When we first
                       met, Nunca imagine que te iba a conocer y que ibas a ser
-                      tan importante para mi😊❤️Pero ahora, no puedo imaginar una
-                      vida sin ti. Eres mi mundo🌎
-                      <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
+                      tan importante para mi😊❤️Pero ahora, no puedo imaginar
+                      una vida sin ti. Eres mi mundo🌎
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          marginTop: 2,
+                          fontFamily: "'Fresh Palm',cursive",
+                          fontSize: "1.2rem",
+                        }}
+                      >
                         I can’t stop thinking about you, mi amor. You mean the
                         world to me, and I love making you happy, laughing, and
                         seeing your beautiful smile😊 I love how smart, caring,
-                        beautiful, lovely, cute, adorable, and honest you are😊 I
-                        love how you’re not afraid to speak your mind.
+                        beautiful, lovely, cute, adorable, and honest you are😊
+                        I love how you’re not afraid to speak your mind.
                       </Typography>
-                      <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          marginTop: 2,
+                          fontFamily: "'Fresh Palm',cursive",
+                          fontSize: "1.2rem",
+                        }}
+                      >
                         I love you so much, mi florecita preciosa😊❤️ I love you
                         more than words can express. I can’t wait to make more
                         memories with you😊❤️
                       </Typography>
-                      <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          marginTop: 2,
+                          fontFamily: "'Fresh Palm',cursive",
+                          fontSize: "1.2rem",
+                        }}
+                      >
                         You are the light of my life and the joy in my heart.
                         Every moment with you is a treasure, and I cherish you
                         more than words can say.
                       </Typography>
-                      <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
-                        Ya se que a veces no soy bueno con las palabras pero quiero que
-                        sepas que te quiero mucho y que eres la mejor cosa que
-                        me ha pasado en mi vida.
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          marginTop: 2,
+                          fontFamily: "'Fresh Palm',cursive",
+                          fontSize: "1.2rem",
+                        }}
+                      >
+                        Ya se que a veces no soy bueno con las palabras pero
+                        quiero que sepas que te quiero mucho y que eres la mejor
+                        cosa que me ha pasado en mi vida.
                       </Typography>
-                      <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          marginTop: 2,
+                          fontFamily: "'Fresh Palm',cursive",
+                          fontSize: "1.2rem",
+                        }}
+                      >
                         I know I haven’t asked you out in person yet, and I know
                         we haven’t had the chance, but I will when we have some
                         time alone or on our date in the city. I hope my
-                        feelings for you have been clear, mi amorcita😊❤️ Te quiero
-                        mucho mi amor y que no se te olvide nunca.
+                        feelings for you have been clear, mi amorcita😊❤️ Te
+                        quiero mucho mi amor y que no se te olvide nunca.
                       </Typography>
                     </Typography>
                     <Typography
                       variant="body1"
-                      sx={{ marginTop: 2, marginLeft: -6,fontFamily: "'Shine in Valentine',cursive",
-                        fontSize: "1.2rem", fontWeight: "bold"}}
+                      sx={{
+                        marginTop: 2,
+                        marginLeft: -6,
+                        fontFamily: "'Shine in Valentine',cursive",
+                        fontSize: "1.2rem",
+                        fontWeight: "bold",
+                      }}
                     >
                       Love,
                     </Typography>
-                    <Typography variant="body1" sx={{ marginTop: -0.5,fontFamily: "'Shine in Valentine',cursive",
-                        fontSize: "1.2rem", fontWeight: "bold"}}>
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        marginTop: -0.5,
+                        fontFamily: "'Shine in Valentine',cursive",
+                        fontSize: "1.2rem",
+                        fontWeight: "bold",
+                      }}
+                    >
                       Tu solecito☀️
                     </Typography>
-                    <Typography variant="body1" sx={{ marginTop: 2,fontFamily: "'Fresh Palm',cursive",
-                        fontSize: "1.2rem", }}>
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        marginTop: 2,
+                        fontFamily: "'Fresh Palm',cursive",
+                        fontSize: "1.2rem",
+                      }}
+                    >
                       P.S. I hope you like this little surprise. I love you so
                       much mi florecita preciosa❤️
                     </Typography>
@@ -422,14 +475,13 @@ export default function SecretLetter() {
         )
       ) : (
         <>
-        <Typography variant="h6" sx={{ marginTop: 2, textAlign: "center" }}>
-          ⏳ Oops! Tendras que esperar until {formattedRevealDate}😊⏳
-        </Typography>
-        <Typography variant="h6" sx={{ marginTop: 2, textAlign: "center" }}>
+          <Typography variant="h6" sx={{ marginTop: 2, textAlign: "center", fontFamily: "'Fresh Palm',cursive" }}>
+            ⏳ Oops! Tendras que esperar until {formattedRevealDate}😊⏳
+          </Typography>
+          <Typography variant="h6" sx={{ marginTop: 2, textAlign: "center", fontFamily: "'Fresh Palm',cursive" }}>
             Countdown: {formatTime(timeLeft)}
           </Typography>
         </>
-        
       )}
     </div>
   );
